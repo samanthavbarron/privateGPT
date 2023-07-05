83bc9152a8513ba5e15a1a6868c8ad426f77b024
Samantha V. Barron <sam@sambarron.me>
2023-07-05 22:14:00 +0000

    add requirements

--- setup.py
+++ setup.py
@@ -10,4 +10,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/imartinez/privateGPT",
     packages=["privateGPT"],
+    install_requires=[
+        "langchain==0.0.197",
+        "gpt4all==0.3.4",
+        "chromadb==0.3.23",
+        "llama-cpp-python==0.1.50",
+        "urllib3==2.0.2",
+        "PyMuPDF==1.22.3",
+        "python-dotenv==1.0.0",
+        "unstructured==0.6.6",
+        "extract-msg==0.41.1",
+        "tabulate==0.9.0",
+        "pandoc==2.3",
+        "pypandoc==1.11",
+        "tqdm==4.65.0",
+    ]
 )
